88cddda40d052895ee3eb99f1457bd9eef90d11e
Sgt Steiner <futitotal@gmail.com>
2017-12-06 19:08:47 +0100

    Inicial commit

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+# from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # post views
+    # url(r'^$', views.post_list, name="post_list"),
+    path('',
+         views.PostListView.as_view(),
+         name="post_list"),
+    path('^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
+         views.post_detail,
+         name="post_detail")
+]
