8dba8276339cfbf756e6df0b6a0714513eab68a2
Sgt Steiner <futitotal@gmail.com>
2017-12-21 20:34:49 +0100

    eliminar comentario código

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-#from django.core.urlresolvers import reverse
 
 
 # Create your models here.
