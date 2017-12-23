4adbf18df9601ccbce3b52de47ddbc32758a4e4b
Sgt Steiner <futitotal@gmail.com>
2017-12-23 12:34:30 +0100

    Adding a sitemap

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,16 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
 
-"""
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include("blog.urls",
-                          namespace="blog",
-                          app_name="blog"))
-]
-"""
+
+sitemaps = {
+    "post": PostSitemap,
+}
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include("blog.urls", namespace="blog")),
+    path('sitemap/sitemap.xml', sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
 ]
